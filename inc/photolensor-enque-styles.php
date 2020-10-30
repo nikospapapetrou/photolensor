9db741df623cf2f3e8d7c4e79f95c6fcd58c18f6
nikos0002 <48804493+nikos0002@users.noreply.github.com>
2020-10-30 17:53:41 +0200

    Second Commit

---
+++ inc/photolensor-enque-styles.php
@@ -0,0 +1,29 @@
+<?php 
+
+function Photolensor_enqueue_styles() {
+
+wp_enqueue_style( 'main-css', get_stylesheet_directory_uri() . 
+'/style.css', [], time(), 'all' );
+wp_enqueue_style( 'web-font',  get_template_directory_uri() .'/assets/web-fonts.css', [], time(), 'all' );
+wp_enqueue_style( 'normalize', get_stylesheet_directory_uri() . 
+'/assets/normalize.css', [], time(), 'all' );
+wp_enqueue_style( 'main', get_stylesheet_directory_uri() . 
+'/assets/main.css', ['normalize'], time(), 'all' ); 
+
+
+}
+add_action ( 'wp_enqueue_scripts', 'Photolensor_enqueue_styles' );
+
+
+//Add JavaScript
+
+function Photolensor_enqueue_scripts() {
+
+  wp_enqueue_script( 'main-js', get_stylesheet_directory_uri() . 
+  '/assets/main.js', [], time(), true );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'Photolensor_enqueue_scripts' );
+
+?>
